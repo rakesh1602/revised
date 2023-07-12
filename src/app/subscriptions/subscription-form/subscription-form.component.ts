61aed5cb7906dfd7b0f53555d62f3049c9290d14
rakesh1602 <rakesh.chavan776@gmail.com>
2023-07-12 17:26:42 +0530

    blog-app-angular

---
+++ src/app/subscriptions/subscription-form/subscription-form.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
+
+@Component({
+  selector: 'app-subscription-form',
+  templateUrl: './subscription-form.component.html',
+  styleUrls: ['./subscription-form.component.scss']
+})
+export class SubscriptionFormComponent {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+
+}
